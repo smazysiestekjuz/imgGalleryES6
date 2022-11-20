e8c49452e76f809b254d8bd53e34dee2236bc076
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-20 12:11:57 +0100

    Cleanup in the code

--- main.js
+++ main.js
@@ -1,44 +1,40 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-var filer = document.getElementById('filter');
+var filter = document.getElementById('filter');
 
 form.addEventListener('submit', addItem);
-
 itemList.addEventListener('click', removeItem);
-filer.addEventListener('keyup', filerItems);
+filter.addEventListener('keyup', filterItems);
 
 function addItem(e){
-    e.preventDefault();
-
-    var newItem = document.getElementById('item').value;
-
-    var li = document.createElement('li');
-    
-    li.className = 'list-group-item';
-
-    li.appendChild(document.createTextNode(newItem));
-
-    var deleteBtn = document.createElement('button');
-
-    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-    deleteBtn.appendChild(document.createTextNode('X'));
-
-    li.appendChild(deleteBtn);
-
-    itemList.appendChild(li);
+  e.preventDefault();
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  itemList.appendChild(li);
 }
-
-function removeItem(e) {
-   if(e.target.classList.contains('delete')) {
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
     if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
-   } 
+  }
 }
-
-function filerItems(e) {
-    var text = e.target.value.toLowerCase();
-    var items = itemList.getElementsByTagName('li');
+function filterItems(e){
+  var text = e.target.value.toLowerCase();
+  var items = itemList.getElementsByTagName('li');
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
 }
