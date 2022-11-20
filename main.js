dc95bf7a541d6ebffea184b4fea075c8035ac97d
Mateusz Pniewski <smazysiestekjuz@gmail.com>
2022-11-20 11:42:40 +0100

    append text node

--- main.js
+++ main.js
@@ -17,7 +17,9 @@ function addItem(e){
     var deleteBtn = document.createElement('button');
 
     deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-    
+
+    deleteBtn.appendChild(document.createTextNode('X'));
+
 
     itemList.appendChild(li);
 }
